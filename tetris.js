11a32d152297cee96fe18509b4ae92315cd5d260
vgiakoumakou <v.giakoumakou@ihu.edu.gr>
2018-04-08 02:20:36 +0300

    Implement functions draw, drawMatrix and update

--- tetris.js
+++ tetris.js
@@ -5,20 +5,42 @@ const context = canvas.getContext('2d');
 // Scale canvas items 20 times
 context.scale(20, 20);
 
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
-
 const matrix = [
 	[0, 0 , 0],	//3 rows for 'T' in order to determine easily the center of it and rotate it
 	[1, 1, 1],
 	[0, 1 , 0],
 ];
 
-matrix.forEach((row, y) => {
-	row.forEach((value, x) => {
-		if (value !== 0) {
-			context.fillStyle = 'red';
-			context.fillRect(x, y, 1, 1);
-		}
+function draw() {
+
+	context.fillStyle = '#000';
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	
+	drawMatrix(player.matrix, player.pos);
+}
+
+function drawMatrix(matrix, offset) {
+	matrix.forEach((row, y) => {
+		row.forEach((value, x) => {
+			if (value !== 0) {
+				context.fillStyle = 'red';
+				context.fillRect(x + offset.x,
+								 y + offset.y, 
+								 1, 1);
+			}
+		});
 	});
-});
+}
+
+function update() {
+	draw();
+	requestAnimationFrame(update);
+}
+
+const player = {
+	pos: {x:5, y: 5},
+	matrix: matrix,
+}
+
+
+update();
