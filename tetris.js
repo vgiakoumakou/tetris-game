1ebf3b2b02f28bee6aace0515c3078a44b0c85e5
vgiakoumakou <v.giakoumakou@ihu.edu.gr>
2018-04-08 03:08:20 +0300

    Detect collision.

--- tetris.js
+++ tetris.js
@@ -19,6 +19,18 @@ function createMatrix(w, h) {
 	return matrix;
 }
 
+function collide(arena, player) {
+	const [m, o] = [player.matrix, player.pos];
+	for (let y = 0; y < m.length; ++y) {
+		for (let x = 0; x < m[y].length; ++x) {
+			if (m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
 function draw() {
 
 	context.fillStyle = '#000';
@@ -53,6 +65,11 @@ function merge(arena, player) {
 
 function playerDrop() {
 	player.pos.y++;
+	if (collide(arena, player) {
+		player.pos.y--;
+		merge(arena, player);
+		player.pos.y = 0;
+	})
 	dropCounter = 0; //we don't want another immediate drop
 }
 
